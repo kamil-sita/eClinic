a087e7666886e0f92758a07a979d91a56782c0f8
rzepkamichal <michrzep@interia.pl>
2019-10-29 19:34:13 +0100

    Upload basic project

---
+++ src/test/java/pl/io/e_clinic/EClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.io.e_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
