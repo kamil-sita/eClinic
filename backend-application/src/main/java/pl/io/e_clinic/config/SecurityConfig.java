3768df2ca7e09b21b99b75e24e46ca71984c63fe
Dominik Wolny <wolny.dominik@outlook.com>
2020-01-09 13:16:57 +0100

    Added VERY basic and VERY alpha authentication
    It just forbids everyone for now.

---
+++ backend-application/src/main/java/pl/io/e_clinic/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package pl.io.e_clinic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    // Tylko localhost:8080/ i localhost:8080/home nie wymaga autoryzacji, do zmiany
+                    .antMatchers("/", "/home").permitAll()
+                    .anyRequest().authenticated();
+                //    .and()
+                //.formLogin()
+                //    // Pod adresem localhost:8080/login jest strona logowaniae
+                //    .loginPage("/login")
+                //    .permitAll()
+                //    .and()
+                //.logout()
+                //    .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
