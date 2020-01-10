bddfcf4380472aeb365cbd2ce126a524a78f70fc
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2020-01-10 20:03:41 +0100

    dziwne rzeczy

--- backend-application/src/main/java/pl/io/e_clinic/controller/employees/EmployeesController.java
+++ backend-application/src/main/java/pl/io/e_clinic/controller/employees/EmployeesController.java
@@ -12,6 +12,7 @@ import pl.io.e_clinic.controller.employees.dto.EmployeeDto;
 import pl.io.e_clinic.entity.employee.model.Employee;
 import pl.io.e_clinic.entity.employee.model.Role;
 import pl.io.e_clinic.entity.employee.repository.EmployeeRepository;
+import pl.io.e_clinic.entity.room.repository.RoomRepository;
 import pl.io.e_clinic.entity.workflowholiday.model.WorkflowHoliday;
 import pl.io.e_clinic.entity.workflowholiday.repository.WorkflowHolidayRepository;
 import pl.io.e_clinic.entity.workflowovertime.model.WorkflowOvertime;
@@ -19,6 +20,9 @@ import pl.io.e_clinic.entity.workflowovertime.repository.WorkflowOvertimeReposit
 import pl.io.e_clinic.entity.workflowsicknote.model.WorkflowSickNote;
 import pl.io.e_clinic.entity.workflowsicknote.repository.WorkflowSickNoteRepository;
 import pl.io.e_clinic.services.FilteringService;
+import pl.io.e_clinic.entity.weekschedule.repository.WeekScheduleRepository;
+import pl.io.e_clinic.entity.weekschedule.model.WeekSchedule;
+import pl.io.e_clinic.entity.weekschedule.model.WeekDay;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,6 +59,12 @@ public class EmployeesController {
 
     @Autowired
     WorkflowOvertimeRepository workflowOvertimeRepository;
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    WeekScheduleRepository weekScheduleRepository;
 
 
     @GetMapping(value = "/{employee_id}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -163,6 +173,81 @@ public class EmployeesController {
                 filteredEmployees.get(0), workflowOvertime)), HttpStatus.ACCEPTED);
     }
 
+    @GetMapping(value = "/{user_id}/visits", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody Set<Visit> getVisits(@PathVariable Long user_id, @RequestParam(value="date", required=false) Date date) {
+        Optional<Employee> employee = employeeRepository.findById(user_id);
+
+        if (!employee.isPresent()) return new HashSet<>();
+
+        Set<Visit> visits = employee.get().getVisits();
+
+        if (date != null) {
+            visits = visits.stream().filter((Visit visit) -> {
+                //sprawdza czy TYLKO DZIEŃ MIESIĄC I ROK są równe
+                Calendar cal1 = Calendar.getInstance();
+                Calendar cal2 = Calendar.getInstance();
+                cal1.setTime(date);
+                cal2.setTime(visit.getScheduledDate());
+                return cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR) &&
+                        cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR);
+            }).collect(Collectors.toSet());
+        }
+
+
+        return visits;
+    }
+
+    @GetMapping(value = "/{employee_id}/schedule", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<WeekSchedule> getSchedule(@PathVariable Long employee_id) {
+
+        //pobranie wszystkiego dla konkretnego pracownika
+
+        long count = weekScheduleRepository.count();
+        Page<WeekSchedule> schedules = weekScheduleRepository.findAll(PageRequest.of(0, (int)count));
+
+        List<WeekSchedule> schedule = schedules.getContent();
+
+        //opcjonalne filtrowanie
+        schedule = new FilteringService<>(schedule)
+                .contains(employee_id, WeekSchedule::getEmployeeId)
+                .getFiltered();
+
+        return schedule;
+        //  return schedule;
+    }
+
+    @PutMapping(value = "/{employee_id}/schedule", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<WeekSchedule>> putSchedule(
+            @PathVariable Long employee_id,
+            @RequestBody List<WeekSchedule> schedule
+    ) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(employee_id);
+
+        //pobranie wszystkiego dla konkretnego pracownika
+
+        long count = weekScheduleRepository.count();
+        Page<WeekSchedule> schedulePage = weekScheduleRepository.findAll(PageRequest.of(0, (int)count));
+
+        List<WeekSchedule> scheduleList = schedulePage.getContent();
+        //filtrowanie do grafiku danego pracownika
+        scheduleList = new FilteringService<>(schedule)
+                .contains(employee_id, WeekSchedule::getEmployeeId)
+                .getFiltered();
+
+        for(WeekSchedule currentSchedule : scheduleList){
+            /*if(currentSchedule.getWeekDay() == WeekDay.PN){
+                // cur
+            }*/
+        }
+
+
+        if (!optionalEmployee.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+
+        return new ResponseEntity<>(null, HttpStatus.ACCEPTED);
+    }
 
    /* @GetMapping(value = "/{user_id}/schedule", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
