468e32f78c90dfdbf1cb62288fea7f82b995695a
sosena1 <micsosna@gmail.com>
2020-01-19 20:13:12 +0100

    Należy jeszcze dodać wprowadzone dane do bazy danych.

--- web-client/src/app/visits/visit-add/visit-add.component.ts
+++ web-client/src/app/visits/visit-add/visit-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup} from "@angular/forms";
 import {Router} from "@angular/router";
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-visit-add',
@@ -9,18 +10,39 @@ import {Router} from "@angular/router";
 })
 export class VisitAddComponent implements OnInit {
 
+  submitted = false;
+
   private addVisitForm = new FormGroup({
 
     });
 
-  constructor(private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.addVisitForm = this.formBuilder.group({
+      patientId: [''],
+      doctorId: [''],
+      firstNamePatient: [''],
+      lastNamePatient: [''],
+      firstNameDoctor: [''],
+      lastNameDoctor: [''],
+      date: [''],
+      time: [''],
+    });
+  }
+  get f() { return this.addVisitForm.controls; }
+
+  addVisitToDB() {
+    this.submitted = true;
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.addVisitForm.value));
+
+    // stop here if form is invalid
+    if (this.addVisitForm.invalid) {
+      return;
+    }
   }
 
-  addVisitToDB(){
-    //Add visit
-  }
 
   returnToVisitList(){
     this.router.navigate(['/visitlist/'])
